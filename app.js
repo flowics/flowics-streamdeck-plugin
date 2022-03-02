ff4d46b5e44b4fcf2835f4cb0a4d9b0240e8a87f
Fernando Zunino <fernando@flowics.com>
2022-03-02 13:17:01 -0300

    format

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$SD.on('connected', (jsonObj) => connected(jsonObj));
+$SD.on("connected", (jsonObj) => connected(jsonObj));
 
 function connected(jsn) {
     $SD.on('gg.datagram.web-requests.http.keyDown', (jsonObj) => sendHttp(jsonObj));
@@ -20,55 +20,55 @@ function connected(jsn) {
  * }} data
  */
 function sendHttp(data) {
-    const { url, method, contentType, headers, body } = data.payload.settings;
-    log('sendHttp', { url, method, contentType, headers, body });
+  const { url, method, contentType, headers, body } = data.payload.settings;
+  log("sendHttp", { url, method, contentType, headers, body });
 
-    let defaultHeaders = contentType ? {
-        'Content-Type':  contentType
-    } : {};
-    let inputHeaders = {};
+  let defaultHeaders = contentType
+    ? {
+        "Content-Type": contentType,
+      }
+    : {};
+  let inputHeaders = {};
 
-    if (headers) {
-        const headersArray = headers.split(/\n/);
+  if (headers) {
+    const headersArray = headers.split(/\n/);
 
-        for (let i = 0; i < headersArray.length; i += 1) {
-            if (headersArray[i].includes(':')) {
-                const [headerItem, headerItemValue] = headersArray[i].split(/:(.*)/);
-                const trimmedHeaderItem = headerItem.trim();
-                const trimmedHeaderItemValue = headerItemValue.trim();
+    for (let i = 0; i < headersArray.length; i += 1) {
+      if (headersArray[i].includes(":")) {
+        const [headerItem, headerItemValue] = headersArray[i].split(/:(.*)/);
+        const trimmedHeaderItem = headerItem.trim();
+        const trimmedHeaderItemValue = headerItemValue.trim();
 
-                if (trimmedHeaderItem) {
-                    inputHeaders[trimmedHeaderItem] = trimmedHeaderItemValue;
-                }
-            }
+        if (trimmedHeaderItem) {
+          inputHeaders[trimmedHeaderItem] = trimmedHeaderItemValue;
         }
+      }
     }
+  }
 
-    const fullHeaders = {
-        ...defaultHeaders,
-        ...inputHeaders
-    }
+  const fullHeaders = {
+    ...defaultHeaders,
+    ...inputHeaders,
+  };
 
-    log(fullHeaders);
+  log(fullHeaders);
 
-    if (!url || !method) {
-        showAlert(data.context);
-        return;
-    }
-    fetch(
-        url,
-        {
-            cache: 'no-cache',
-            headers: fullHeaders,
-            method,
-            body: ['GET', 'HEAD'].includes(method) ? undefined : body,
-        })
-        .then(checkResponseStatus)
-        .then(() => showOk(data.context))
-        .catch(err => {
-            showAlert(data.context);
-            logErr(err);
-        });
+  if (!url || !method) {
+    showAlert(data.context);
+    return;
+  }
+  fetch(url, {
+    cache: "no-cache",
+    headers: fullHeaders,
+    method,
+    body: ["GET", "HEAD"].includes(method) ? undefined : body,
+  })
+    .then(checkResponseStatus)
+    .then(() => showOk(data.context))
+    .catch((err) => {
+      showAlert(data.context);
+      logErr(err);
+    });
 }
 
 /**
@@ -83,24 +83,26 @@ function sendHttp(data) {
  * }} data
  */
 function sendWebSocket(data) {
-    const { url, body } = data.payload.settings;
-    log('sendWebSocket', { url, body });
-    if (!url || !body) {
-        showAlert(data.context);
-        return;
-    }
-    const ws = new WebSocket(url);
-    ws.onerror = err => {
-        showAlert(data.context);
-        logErr(new Error('WebSocket error occurred'));
-    };
-    ws.onclose = function(evt) { onClose(this, evt); };
-    ws.onopen = function() {
-        onOpen(this);
-        ws.send(body);
-        ws.close();
-        showOk(data.context);
-    };
+  const { url, body } = data.payload.settings;
+  log("sendWebSocket", { url, body });
+  if (!url || !body) {
+    showAlert(data.context);
+    return;
+  }
+  const ws = new WebSocket(url);
+  ws.onerror = (err) => {
+    showAlert(data.context);
+    logErr(new Error("WebSocket error occurred"));
+  };
+  ws.onclose = function (evt) {
+    onClose(this, evt);
+  };
+  ws.onopen = function () {
+    onOpen(this);
+    ws.send(body);
+    ws.close();
+    showOk(data.context);
+  };
 }
 
 /**
@@ -108,20 +110,20 @@ function sendWebSocket(data) {
  * @returns {Promise<Response>}
  */
 async function checkResponseStatus(resp) {
-    if (!resp) {
-        throw new Error();
-    }
-    if (!resp.ok) {
-        throw new Error(`${resp.status}: ${resp.statusText}\n${await resp.text()}`);
-    }
-    return resp;
+  if (!resp) {
+    throw new Error();
+  }
+  if (!resp.ok) {
+    throw new Error(`${resp.status}: ${resp.statusText}\n${await resp.text()}`);
+  }
+  return resp;
 }
 
 /**
  * @param {WebSocket} ws
  */
 function onOpen(ws) {
-    log(`Connection to ${ws.url} opened`);
+  log(`Connection to ${ws.url} opened`);
 }
 
 /**
@@ -129,37 +131,37 @@ function onOpen(ws) {
  * @param {CloseEvent} evt
  */
 function onClose(ws, evt) {
-    log(`Connection to ${ws.url} closed:`, evt.code, evt.reason);
+  log(`Connection to ${ws.url} closed:`, evt.code, evt.reason);
 }
 
 /**
  * @param {string} context
  */
 function showOk(context) {
-    $SD.api.showOk(context);
+  $SD.api.showOk(context);
 }
 
 /**
  * @param {string} context
  */
 function showAlert(context) {
-    $SD.api.showAlert(context);
+  $SD.api.showAlert(context);
 }
 
 /**
  * @param {...unknown} msg
  */
 function log(...msg) {
-    console.log(...msg);
-    $SD.api.logMessage(msg.map(stringify).join(' '));
+  console.log(...msg);
+  $SD.api.logMessage(msg.map(stringify).join(" "));
 }
 
 /**
  * @param {...unknown} msg
  */
 function logErr(...msg) {
-    console.error(...msg);
-    $SD.api.logMessage(msg.map(stringify).join(' '));
+  console.error(...msg);
+  $SD.api.logMessage(msg.map(stringify).join(" "));
 }
 
 /**
@@ -167,8 +169,8 @@ function logErr(...msg) {
  * @returns {string}
  */
 function stringify(input) {
-    if (typeof input !== 'object' || input instanceof Error) {
-        return input.toString();
-    }
-    return JSON.stringify(input, null, 2);
+  if (typeof input !== "object" || input instanceof Error) {
+    return input.toString();
+  }
+  return JSON.stringify(input, null, 2);
 }
