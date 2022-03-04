3faa2a9b4a64e783eae403d90fdd0ad2c8f4b977
Fernando Zunino <fernando@flowics.com>
2022-03-04 13:12:13 -0300

    Adds Take In, Take Out and Take All Out actions

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ $SD.on("connected", (jsonObj) => connected(jsonObj));
 
 function connected(jsn) {
   $SD.on("com.flowics.graphics.overlay.toggle.keyDown", (jsonObj) =>
-    sendToggle(jsonObj)
+    sendOverlayAction("toggle", jsonObj)
+  );
+  $SD.on("com.flowics.graphics.overlay.takein.keyDown", (jsonObj) =>
+    sendOverlayAction("in", jsonObj)
+  );
+  $SD.on("com.flowics.graphics.overlay.takeout.keyDown", (jsonObj) =>
+    sendOverlayAction("out", jsonObj)
+  );
+  $SD.on("com.flowics.graphics.overlay.takeallout.keyDown", (jsonObj) =>
+    sendTakeAllOut(jsonObj)
   );
 }
 
 /**
+ * @param {'toggle' | 'in' | 'out'} overlayAction
  * @param {{
  *   context: string,
  *   payload: {
@@ -17,12 +27,12 @@ function connected(jsn) {
  *   },
  * }} data
  */
-function sendToggle(data) {
+function sendOverlayAction(overlayAction, data) {
   const url = `https://api.flowics.com/graphics/${data.payload.settings.token}/control/overlays/transition`;
   const body = JSON.stringify([
     {
       id: data.payload.settings.overlayId,
-      transition: "toggle",
+      transition: overlayAction,
     },
   ]);
   const newData = {
@@ -33,6 +43,30 @@ function sendToggle(data) {
         method: "PUT",
         contentType: "application/json",
         body,
+      },
+    },
+  };
+  sendHttp(newData);
+}
+
+/**
+ * @param {{
+ *   context: string,
+ *   payload: {
+ *     settings: {
+ *       token?: string,
+ *     }
+ *   },
+ * }} data
+ */
+function sendTakeAllOut(data) {
+  const url = `https://api.flowics.com/graphics/${data.payload.settings.token}/control/overlays/take-all-out`;
+  const newData = {
+    ...data,
+    payload: {
+      settings: {
+        url,
+        method: "PUT",
       },
     },
   };
